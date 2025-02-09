b289d535ad4733aad9a390a3e78272b006bda727
Anuj192006 <anuju712@gmail.com>
2025-02-09 16:49:00 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuIcon = document.querySelector("#menu-icon");
+let nav = document.getElementById("my-nav")
+
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x');
+    nav.classList.toggle("active");
+    
+}
